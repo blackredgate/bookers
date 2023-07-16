3598f5fa4036c0d86037031dc3cd8c1deaf34180
blackredgate <138416861+blackredgate@users.noreply.github.com>
2023-07-16 07:40:56 +0000

    [Fix]新規投稿機能、Backのリンク修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,24 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
-  
+
   def create
-    book = Book.new(book_params)
-    book.save
+    @book = Book.new(book_params)
+    @book.save
     redirect_to '/books'
   end
-  
+
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
